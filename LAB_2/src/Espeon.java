d3b546340eb03f49d77d2aa834a5d955c96c4806
dmittrey <0704Vs2910>
2021-02-19 04:07:49 +0300

    2 step

--- LAB_2/src/Espeon.java
+++ LAB_2/src/Espeon.java
@@ -3,8 +3,8 @@ import ru.ifmo.se.pokemon.*;
 public class Espeon extends Pokemon{
     public Espeon(String name, int level){
         super(name, level);
-        setStats(65,65, 60, 130,95,110);
+        setStats(65,65, 60, 130,95,110);//
         setType(Type.PSYCHIC);
-        setMove(new Covet, new HyperVoice, new IrnoTail, new ZenHeadbutt);
+        setMove(new QuickAttack(), new TailWhip(), new Confide(), new DreamEater());
     }
 }
