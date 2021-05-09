1c540ccd9af17f90b89ddff3a8ef564538ef668e
dmittrey <dmitryzubakhin@mail.ru>
2021-05-09 21:36:10 +0300

    Info and CollectionManagerInterface(for D from SOLID)

--- Lab5/src/utility/CollectionManager.java
+++ Lab5/src/utility/CollectionManager.java
@@ -1,30 +1,52 @@
 package utility;
 
-import utility.Console;
-import java.time.ZonedDateTime;
-import java.util.HashSet;
+import data.StudyGroup;
 
-public class CollectionManager {
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class CollectionManager implements CollectionManagerInterface {
 
     private final HashSet<data.StudyGroup> studyGroups;
 
     private boolean exeDone;
 
-    private final ZonedDateTime initTime;
+    private final LocalDate initTime;
 
-    private final Console console;
-
-    public CollectionManager(Console aConsole) {
+    public CollectionManager() {
         studyGroups = new HashSet<>();
-        initTime = ZonedDateTime.now();
-        console = aConsole;
+        initTime = LocalDate.now();
     }
 
-    /**
-     *
-     *
-     * Тут будут распологаться команды для взимодействия к коллекцией,
-     * но при этом они будут не полностью обрабатывать элементы а просто возвращать требуемое
-     *
-     */
+    public String getInfo() {
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("\t").append("Type of collection:").append("\t\t\t\t\t").append("HashSet").append("\n");
+        sb.append("\t").append("Type of collection items:").append("\t\t\t").append("Study groups").append("\n");
+        sb.append("\t").append("Priority:").append("\t\t\t\t\t\t\t").append("Average mark").append("\n");
+        sb.append("\t").append("Initialization date:").append("\t\t\t\t").append(initTime).append("\n");
+        sb.append("\t").append("Number of items in te collection:").append("\t").append(this.getCount()).append("\n");
+        return sb.toString();
+    }
+
+    private int getCount() {
+        /**
+         * Прочитать про итератор по нормальному
+         */
+
+        Iterator<StudyGroup> iterator = studyGroups.iterator();
+        int i = 0;
+        while (iterator.hasNext()) {
+            System.out.println();
+            i++;
+        }
+        return i;
+    }
+
+
+
 }
